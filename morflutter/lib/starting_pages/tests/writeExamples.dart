d8139e56cbaa74ed897c92fe85b748b78d2d63fd
binivazqua <binizavazquez@gmail.com>
2024-06-02 18:47:37 -0600

    real time database write data with log done

--- morflutter/lib/starting_pages/tests/writeExamples.dart
+++ morflutter/lib/starting_pages/tests/writeExamples.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 /* FIREBASE REQUIRED IMPORTS */
@@ -15,6 +17,18 @@ class _WriteExamplesState extends State<WriteExamples> {
   /* HOW TO SEND DATA TO REAL TIME DATABASE:
 
       1. Create an ainstance of our database.
+        // a path to our real time database, wich when initialized,
+        points to the top of our tree.
+      2. Create a child to my reference inside my builder, wich will 
+      refer (xd?) to my branches.
+      3. Write data using <set> method.
+        // This should be done in a JSON format, as follows:
+        {
+          'key': 'data',
+          'key1': 'data1',
+          'key3': 'data3',
+        }
+      
 
   */
 
@@ -23,6 +37,10 @@ class _WriteExamplesState extends State<WriteExamples> {
 
   @override
   Widget build(BuildContext context) {
+    // STEP #2:
+    final testWrite =
+        database.child('testWrite/'); // MAKE SURE U HAVE '/' in there!
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Write Examples'),
@@ -31,7 +49,47 @@ class _WriteExamplesState extends State<WriteExamples> {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
-          children: [],
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  // PROPER SYNTAXIS TO WRITE DATA IN A JSON FORMAT.
+                  testWrite
+                      .set({
+                        'child1': 'value1',
+                        'child2': 'value2',
+                        'child3': 'value3',
+                      })
+                      .then((_) => print('Data values have been sent!'))
+                      .catchError((error) =>
+                          print('Something didnt work! Error: $error'));
+                },
+                child: Text('Press to send')),
+            ElevatedButton(
+                onPressed: () {
+                  // PROPER SYNTAXIS TO CHANGE DATA.
+                  testWrite
+                      .child('/child1/')
+                      .set('another value')
+                      .then((_) => print('Data values have been CHANGED!'))
+                      .catchError((error) =>
+                          print('Something didnt work! Error: $error'));
+                  ;
+                },
+                child: Text('Press to change')),
+            ElevatedButton(
+                onPressed: () {
+                  // PROPER SYNTAXIS TO CHANGE/REPLACE DATA.
+                  testWrite
+                      .update({
+                        'child2': 'value now equals = ${Random().nextInt(100)}'
+                      })
+                      .then((_) => print('Data values have been UPDATED!'))
+                      .catchError((error) =>
+                          print('Something didnt work! Error: $error'));
+                  ;
+                },
+                child: Text('Press to update')),
+          ],
         ),
       ),
     );
