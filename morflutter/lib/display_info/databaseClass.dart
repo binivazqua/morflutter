4763771b7992322ade3cd9708f6919e16006d3cd
binivazqua <binizavazquez@gmail.com>
2024-07-21 09:58:28 -0600

    MorfoData class model improvement

--- morflutter/lib/display_info/databaseClass.dart
+++ morflutter/lib/display_info/databaseClass.dart
@@ -1,11 +1,34 @@
+/**
+ * Create a MuscleReading class to parse our muscle data with filters:
+ */
+class MuscleReading {
+  final String muscle;
+  final String value;
+
+  MuscleReading({required this.muscle, required this.value});
+}
+
+/**
+ * Our database model to gather values in a structured way:
+ */
 class MorfoData {
   final String time;
-  final Map<String, dynamic> muscleData;
+  final List<MuscleReading> muscleData;
 
   MorfoData({required this.time, required this.muscleData});
 
   // Factory constructor to create a SensorData object from a map:
   factory MorfoData.fromMap(String time, Map<String, dynamic> muscleData) {
-    return MorfoData(time: time, muscleData: muscleData);
+    List<MuscleReading> muscleReadings = [];
+
+    muscleData.forEach((muscle, readings) {
+      if (readings is Map<dynamic, dynamic>) {
+        readings.forEach((key, value) {
+          muscleReadings.add(MuscleReading(muscle: muscle, value: value));
+        });
+      }
+    });
+
+    return MorfoData(time: time, muscleData: muscleReadings);
   }
 }
