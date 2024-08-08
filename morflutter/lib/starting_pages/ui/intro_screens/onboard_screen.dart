1e5a36895f7f2e52a2ebbf5fe52f1b24b93cd933
binivazqua <binizavazquez@gmail.com>
2024-08-08 12:21:03 -0600

    smoothpageindicator

--- morflutter/lib/starting_pages/ui/intro_screens/onboard_screen.dart
+++ morflutter/lib/starting_pages/ui/intro_screens/onboard_screen.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:morflutter/design/constants.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingScreen extends StatelessWidget {
-  const OnBoardingScreen({super.key});
+  PageController _pageController = new PageController();
+  OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(children: [
+      body: Stack(children: [
+        PageView(children: [
+          Container(
+            color: lilyPurple,
+          ),
+          Container(color: darkPeriwinkle),
+          Container(color: draculaPurple),
+        ]),
         Container(
-          color: Colors.purple,
-        ),
-        Container(),
-        Container(),
+            alignment: Alignment(0, 0),
+            child: SmoothPageIndicator(controller: _pageController, count: 3)),
       ]),
+
+      // DOT INDICATORS
     );
   }
 }
